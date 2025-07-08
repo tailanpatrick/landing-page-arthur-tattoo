369a8effb705a792018afe9a4d814ca9182d8788
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-07-08 12:09:41 -0300

    ajustado banner para mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,12 +19,12 @@ const Hero = () => {
 	const { title, subtitle, btnText, btnIcon } = heroData;
 
 	return (
-		<section className="bg-hero bg-cover bg-center min-h-[40vh] lg:h-[948px] bg-no-repeat relative mt-[120px] lg:mt-[150px]">
+		<section className="bg-hero bg-cover bg-center min-h-[80vh] lg:h-[948px] bg-no-repeat relative mt-[120px] lg:mt-[-10px]">
 			<motion.div
 				variants={container}
 				initial="hidden"
 				whileInView={'show'}
-				className="container mx-auto min-h-[40vh] lg:h-full flex items-center justify-center xl:justify-end"
+				className="container mx-auto min-h-[80vh] lg:h-full flex items-center justify-center xl:justify-end"
 			>
 				<div className="text-white text-center lg:text-left lg:max-w-[640px]">
 					<motion.h1 variants={fadeIn('down')} className="h1">
