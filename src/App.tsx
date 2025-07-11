ebf4f7c51e9aa63ba81737bbbd3cfcbbc0c67a28
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-07-11 19:36:47 -0300

    removida página de testimonial

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,25 @@ import About from './components/About';
 import GallerySection from './components/GallerySection';
 import Skills from './components/Skills';
 import Interview from './components/Interview';
-import Testimonial from './components/Testimonial';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Copyright from './components/Copyright';
 
 const App = () => {
-  return (
-    <div className='max-w-[1920px] mx-auto overflow-hidden bg-white'>
-      <Header />
-      <Hero />
-      <About />
-      <GallerySection />
-      <Skills />
-      <Testimonial />
-      <Interview />
-      <Contact />
-      <Footer />
-      <Copyright />
-      {/* <div className='h-[4000px]'></div> */}
-    </div>
-  );
+	return (
+		<div className="max-w-[1920px] mx-auto overflow-hidden bg-white">
+			<Header />
+			<Hero />
+			<About />
+			<GallerySection />
+			<Skills />
+			<Interview />
+			<Contact />
+			<Footer />
+			<Copyright />
+			{/* <div className='h-[4000px]'></div> */}
+		</div>
+	);
 };
 
 export default App;
