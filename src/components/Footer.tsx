9b2c0c75db96b71db5cc5588ca83ba17dc036cae
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-07-15 10:19:34 -0300

    criada seção de rodapé do site

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,84 @@
 import React from 'react';
+import { footerData } from '../data';
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+
+const staggerContainer = {
+	hidden: {},
+	show: {
+		transition: {
+			staggerChildren: 0.3,
+			delayChildren: 0.2,
+		},
+	},
+};
 
 const Footer = () => {
-  return <div>Footer</div>;
+	const { about, links, newsletter, program } = footerData;
+	return (
+		<footer className="section bg-dark">
+			<div className="container mx-auto">
+				<motion.div
+					variants={staggerContainer}
+					initial="hidden"
+					whileInView={'show'}
+					viewport={{ once: false, amount: 0.1 }}
+					className="text-white flex flex-col lg:flex-row lg:justify-between gap-x-5 gap-y-14"
+				>
+					<motion.div
+						variants={fadeIn('up')}
+						className="flex-1 flex flex-col gap-y-6"
+					>
+						<div className="font-primary text-xl tracking-[0.08em] uppercase">
+							{about.title}
+						</div>
+						<div className="leading-relaxed text-[#dbdbdb] ">
+							{about.subtitle}
+						</div>
+
+						<div className="flex flex-col gap-y-4 font-semibold text-[#dbdbdb">
+							<div className="flex items-center gap-x-[10px]">
+								<div>{about.address.icon}</div>
+								<div>{about.address.name}</div>
+							</div>
+							<div className="flex items-center gap-x-[10px]">
+								<div>{about.phone.icon}</div>
+								<div>{about.phone.number}</div>
+							</div>
+						</div>
+					</motion.div>
+
+					<motion.div
+						variants={fadeIn('up')}
+						className="flex-1 flex flex-col xl:items-center"
+					>
+						<div>
+							<div className="font-primary text-xl uppercase tracking-[0.08em] mb-6">
+								{links.title}
+							</div>
+							<ul className="flex flex-col gap-y-4 text-[#dbdbdb]">
+								{links.items.map((item, index) => {
+									const { name, href } = item;
+
+									return (
+										<li key={index}>
+											<a
+												href={href}
+												className="hover:text-white transition"
+											>
+												{name}
+											</a>
+										</li>
+									);
+								})}
+							</ul>
+						</div>
+					</motion.div>
+				</motion.div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
